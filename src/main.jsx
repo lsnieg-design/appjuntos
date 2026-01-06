25358a887073e29425607b17d3ef96b079178f45
lsnieg-design <lsnieg@raspanti.edu.ar>
2026-01-05 21:17:54 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App.jsx' // <--- Fíjate que aquí llame a App.jsx (o srcApp.jsx si no le cambiaste el nombre)
+import './index.css' // (Opcional, solo si tienes estilos)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
