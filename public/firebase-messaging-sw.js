1caf5f72df5e706bac157292c2671d523fbdd9c7
lsnieg-design <lsnieg@raspanti.edu.ar>
2026-02-05 19:42:09 -0300

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,12 +4,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-comp
 
 // 1. Pega aquí ABAJO tu configuración de Firebase (la misma que tienes en tu App.js)
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO_ID",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyAsa-o1ykRaY4sy4AhSSrCFApWh-XBhb8M",
+  authDomain: "juntos-a-la-par-d3534.firebaseapp.com",
+  projectId: "juntos-a-la-par-d3534",
+  storageBucket: "juntos-a-la-par-d3534.firebasestorage.app",
+  messagingSenderId: "320753617430",
+  appId: "1:320753617430:web:964ae79ff0ea94dc520337",
+  measurementId: "G-E834BP6PJ6"
 };
 
 firebase.initializeApp(firebaseConfig);
