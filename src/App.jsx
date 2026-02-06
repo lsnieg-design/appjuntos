34d9c4e86bb52a147e9c4334625d783a7f541db1
lsnieg-design <lsnieg@raspanti.edu.ar>
2026-02-06 10:07:27 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1592,10 +1592,10 @@ function MatriculaView({ user }) {
         </span>
     )}
 
-    {/* ETIQUETA AULA (CON ARREGLO PARA QUE NO SE PONGA EN BLANCO) */}
+   {/* 3. ETIQUETA AULA (CON ARREGLO ANTIBLOQUEO) */}
     {s.classroom && (
         <span className="text-[10px] bg-yellow-100 text-yellow-800 px-2 py-0.5 rounded border border-yellow-200 font-bold flex items-center gap-1 shadow-sm">
-            {/* Usamos el código SVG directo para evitar errores de referencia */}
+            {/* Usamos el código directo del ícono para que no de error de referencia */}
             <svg width="10" height="10" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
             Aula {s.classroom}
         </span>
@@ -2173,6 +2173,7 @@ function ActivityLogView() {
     </div>
   );
 }
+
 
 
 
