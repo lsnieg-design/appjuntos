5da6276ac675205802f2b00dbe460eb904831457
lsnieg-design <lsnieg@raspanti.edu.ar>
2026-02-19 09:10:06 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2794,10 +2794,7 @@ const handleImportStaff = async (e) => {
                         <p className="text-[10px] font-bold text-gray-400 uppercase">DNI: <span className="text-gray-600">{s.dni || '-'}</span></p>
                         <p className="text-[10px] font-bold text-gray-400 uppercase truncate">OS: <span className="text-gray-600">{s.healthInsurance || 'NO DECLARA'}</span></p>
                     </div>
-                </div>
-            </div>
-        </div>
-    ))}
+                  ))}
               </div>
           </div>
         </div>
@@ -2882,6 +2879,7 @@ function NavButton({ active, onClick, icon, label }) {
 
 // 2. Icono auxiliar para "Mi Aula"
 const StartIcon = ({size}) => <svg width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>;
+
 
 
 
