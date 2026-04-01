d322d34d0df619f8129d22ebc30f9deba3e39537
lsnieg-design <lsnieg@raspanti.edu.ar>
2026-04-01 12:04:40 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,10 @@ import {
   Send, Key, Filter, LayoutDashboard, Link as LinkIcon, ExternalLink, 
   AlertTriangle, Clock, Shield, Crown, Activity, Share, PlusSquare, 
   Smartphone, GraduationCap, Search, X, UploadCloud, PieChart, Eye, Edit3, Trophy,
-  Folder, MessageSquare, Globe, BookOpen, Lightbulb, ChevronDown, PlusCircle, Printer,
+  Folder, MessageSquare, Globe, BookOpen, Lightbulb, ChevronDown, PlusCircle, Printer,Phone, 
+  BookOpen, 
+  CheckCircle2,
+  Check
   // Iconos específicos para el formato de las notas
   AlignLeft, AlignCenter, AlignRight, AlignJustify, FileSquare
 } from 'lucide-react';
