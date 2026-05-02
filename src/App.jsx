7f94b7862eb961a4811d5c3297e39b063272b282
lsnieg-design <lsnieg@raspanti.edu.ar>
2026-05-02 09:18:04 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import {
   Calendar as CalendarIcon, CheckSquare, Settings, User, FileText, CheckCircle, 
   Download, RefreshCw, Plus, Trash2, Users, AlertCircle, LogOut, Briefcase, 
   Lock, List, Grid, ChevronLeft, ChevronRight, Bell, Check, HelpCircle, Mail, Camera, MapPin, 
-  Send, Key, Filter, LayoutDashboard, Link as LinkIcon, ExternalLink, 
+  Send, Key, Filter, LayoutDashboard, Link as LinkIcon, ExternalLink, Zap,
   AlertTriangle, Clock, Shield, Crown, Activity, Share, PlusSquare, 
   Smartphone, GraduationCap, Search, X, UploadCloud, PieChart, Eye, Edit3, Trophy,
   Folder, MessageSquare, Globe, BookOpen, Lightbulb, ChevronDown, PlusCircle, Printer,
