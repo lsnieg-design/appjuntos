ffed9bdb493d539507057bd30999d054501b8e4d
lsnieg-design <lsnieg@raspanti.edu.ar>
2026-05-05 11:19:11 -0300

    Update ResourcesView.jsx

--- src/views/ResourcesView.jsx
+++ src/views/ResourcesView.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 import { 
-  PlusCircle, Edit3, ChevronRight, ExternalLink, 
-  Trash2, X, List, AlignLeft, AlignCenter, 
-  AlignJustify, Download, FileText 
+  Calendar as CalendarIcon, CheckSquare, Settings, User, FileText, CheckCircle, 
+  Download, RefreshCw, Plus, Trash2, Users, AlertCircle, LogOut, Briefcase, 
+  Lock, List, Grid, ChevronLeft, ChevronRight, Bell, Check, HelpCircle, Mail, Camera, MapPin, 
+  Send, Key, Filter, LayoutDashboard, Link as LinkIcon, ExternalLink, Zap,
+  AlertTriangle, Clock, Shield, Crown, Activity, Share, PlusSquare, 
+  Smartphone, GraduationCap, Search, X, UploadCloud, PieChart, Eye, Edit3, Trophy,
+  Folder, MessageSquare, Globe, BookOpen, Lightbulb, ChevronDown, PlusCircle, Printer,
+  AlignLeft, AlignCenter, AlignRight, AlignJustify, Phone, CheckCircle2, Clock3, UserCheck,
+  ChevronUp // <--- ESTE ES EL QUE FALTABA
 } from 'lucide-react';
 import { 
   collection, addDoc, doc, updateDoc, deleteDoc, serverTimestamp 
