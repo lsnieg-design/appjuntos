2dd74750811bfeb6e3d3b6b001d03f98d43d0377
lsnieg-design <lsnieg@raspanti.edu.ar>
2026-05-05 11:18:16 -0300

    Update CalendarView.jsx

--- src/views/CalendarView.jsx
+++ src/views/CalendarView.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
 import { 
-  ChevronLeft, ChevronRight, Plus, X, 
-  RefreshCw, FileText, ExternalLink, Lock 
+  Calendar as CalendarIcon, CheckSquare, Settings, User, FileText, CheckCircle, 
+  Download, RefreshCw, Plus, Trash2, Users, AlertCircle, LogOut, Briefcase, 
+  Lock, List, Grid, ChevronLeft, ChevronRight, Bell, Check, HelpCircle, Mail, Camera, MapPin, 
+  Send, Key, Filter, LayoutDashboard, Link as LinkIcon, ExternalLink, Zap,
+  AlertTriangle, Clock, Shield, Crown, Activity, Share, PlusSquare, 
+  Smartphone, GraduationCap, Search, X, UploadCloud, PieChart, Eye, Edit3, Trophy,
+  Folder, MessageSquare, Globe, BookOpen, Lightbulb, ChevronDown, PlusCircle, Printer,
+  AlignLeft, AlignCenter, AlignRight, AlignJustify, Phone, CheckCircle2, Clock3, UserCheck,
+  ChevronUp // <--- ESTE ES EL QUE FALTABA
 } from 'lucide-react';
 import { 
   collection, addDoc, doc, updateDoc, 
