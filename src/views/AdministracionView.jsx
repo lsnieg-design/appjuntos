49d95be998ec80fc0a865fcead881de3b898e65a
lsnieg-design <lsnieg@raspanti.edu.ar>
2026-05-05 11:18:09 -0300

    Update AdministracionView.jsx

--- src/views/AdministracionView.jsx
+++ src/views/AdministracionView.jsx
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { 
-  Search, CheckSquare, Printer, RefreshCw, 
-  FileText, Download 
+  Calendar as CalendarIcon, CheckSquare, Settings, User, FileText, CheckCircle, 
+  Download, RefreshCw, Plus, Trash2, Users, AlertCircle, LogOut, Briefcase, 
+  Lock, List, Grid, ChevronLeft, ChevronRight, Bell, Check, HelpCircle, Mail, Camera, MapPin, 
+  Send, Key, Filter, LayoutDashboard, Link as LinkIcon, ExternalLink, Zap,
+  AlertTriangle, Clock, Shield, Crown, Activity, Share, PlusSquare, 
+  Smartphone, GraduationCap, Search, X, UploadCloud, PieChart, Eye, Edit3, Trophy,
+  Folder, MessageSquare, Globe, BookOpen, Lightbulb, ChevronDown, PlusCircle, Printer,
+  AlignLeft, AlignCenter, AlignRight, AlignJustify, Phone, CheckCircle2, Clock3, UserCheck,
+  ChevronUp // <--- ESTE ES EL QUE FALTABA
 } from 'lucide-react';
 import { 
   collection, query, orderBy, onSnapshot 
