b2fb76c99fde096694ceda2b26d1b0c17f61abfd
lsnieg-design <lsnieg@raspanti.edu.ar>
2026-05-08 09:27:39 -0300

    Update GroupsView.jsx

--- src/views/GroupsView.jsx
+++ src/views/GroupsView.jsx
@@ -319,7 +319,7 @@ return (
         >
           <ChevronRight size={32} strokeWidth={3} />
         </button>
-      </div> {/* <--- ESTA ES LA ETIQUETA QUE FALTA CERRAR. Agregala acá. */}
+      </div> 
 
       {selectedStudent && (
         <StudentDetailView student={selectedStudent} user={user} db={db} appId={appId} onClose={() => setSelectedStudent(null)} onEdit={(s) => { setSelectedStudent(null); setFullFileStudent(s); }} />
