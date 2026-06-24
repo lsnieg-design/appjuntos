1653c1c14a07fc0241df90d91f70137cd54b594c
lsnieg-design <lsnieg@raspanti.edu.ar>
2026-06-24 14:29:09 -0300

    Update InformesView.jsx

--- src/views/InformesView.jsx
+++ src/views/InformesView.jsx
@@ -950,8 +950,7 @@ const indicadoresActuales = (tipoInforme === 'musica_brenda' && !esNivelValidoBr
   ? ['2° Ciclo', 'CFI'] 
   : ['Inicial', '1° Ciclo', '2° Ciclo', 'CFI'];
 
- return (
-  <div className="max-w-4xl mx-auto p-4 pb-20 animate-in fade-in relative">
+
    
 return (
   <div className="max-w-4xl mx-auto p-4 pb-20 animate-in fade-in relative">
