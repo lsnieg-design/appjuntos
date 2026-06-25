63fd3dd4f676f2b15ded12f67237df51929229f2
lsnieg-design <lsnieg@raspanti.edu.ar>
2026-06-25 09:22:42 -0300

    Update GroupsView.jsx

--- src/views/GroupsView.jsx
+++ src/views/GroupsView.jsx
@@ -1010,8 +1010,11 @@ return (
     
     <select name="profePlastica" defaultValue={editingGroup.profePlastica || ""} className="p-2 bg-gray-50 rounded-lg text-xs font-bold border">
       <option value="">Plástica...</option>
-    {usersList.filter(u => u.role === ' Profes Especiales').map(u => <option key={u.id} value={u.fullName}>{u.fullName}</option>)}
-    </select>
+   {usersList
+    .filter(u => u.role && u.role.toString().toUpperCase().includes("PROFE ESPECIAL"))
+    .map(u => <option key={u.id} value={u.fullName}>{u.fullName}</option>)
+  }
+</select>
 
     <select name="profeMusica" defaultValue={editingGroup.profeMusica || ""} className="p-2 bg-gray-50 rounded-lg text-xs font-bold border">
       <option value="">Música...</option>
